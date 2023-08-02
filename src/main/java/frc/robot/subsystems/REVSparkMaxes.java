37da9fa09d1d4322dc0d7eb37fac3c905c00c466
theodoreVa <123133002+theodoreVa@users.noreply.github.com>
2023-08-01 20:13:09 -0500

    Made it better

--- src/main/java/frc/robot/subsystems/REVSparkMaxes.java
+++ src/main/java/frc/robot/subsystems/REVSparkMaxes.java
@@ -5,15 +5,36 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class REVSparkMaxes extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
-  public REVSparkMaxes() {
-    private CANSparkMax m_leftMotor;
-    private CANSparkMax m_rightMotor;
-  
+
+CANSparkMax m_leftBackMotor;
+CANSparkMax m_leftFrontMotor;
+CANSparkMax m_rightBackMotor;
+CANSparkMax m_rightFrontMotor;
+
+  public REVSparkMaxes() {  
+    m_leftFrontMotor = new CANSparkMax(4, MotorType.kBrushless);
+    m_leftBackMotor = new CANSparkMax(3, MotorType.kBrushless);
+    m_rightFrontMotor = new CANSparkMax(5, MotorType.kBrushless);
+    m_rightBackMotor = new CANSparkMax(2, MotorType.kBrushless);
+
+    /**
+     * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * parameters will not persist between power cycles
+     */
+    
+     m_leftFrontMotor.restoreFactoryDefaults();
+    m_leftBackMotor.restoreFactoryDefaults();
+    m_rightFrontMotor.restoreFactoryDefaults();
+    m_rightBackMotor.restoreFactoryDefaults();
 }
 
 
@@ -32,7 +53,6 @@ public class REVSparkMaxes extends SubsystemBase {
   }
 
 
-publ
 
   /**
    * An example method querying a boolean state of the subsystem (for example, a digital sensor).
